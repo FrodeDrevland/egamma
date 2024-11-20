Corrected errors in Params function for Beta parameter calculation in the scenario where high and most likely estimates are equal.

egamma/functions.py
@@ -255,10 +255,16 @@ def params(low, most_likely, high, low_prob=0.1):
     else:
         alpha = __find_alpha(low, most_likely, high, low_prob)
 
-    if(most_likely != low):
-        beta = (most_likely - low) / ((alpha - 1) - ppf(low_prob, alpha))
+    if(most_likely-low > high-most_likely):
+        beta_temp=-1
     else:
-        beta = (most_likely - high) / ((alpha - 1) - ppf(1-low_prob, alpha))
+        beta_temp=1
+
+
+    if(most_likely != low):
+        beta = (most_likely - low) / ((alpha - 1) - ppf(low_prob, alpha, beta_temp))
+    else:
+        beta = (most_likely - high) / ((alpha - 1) - ppf(1-low_prob, alpha, beta_temp))
     if abs(beta) == 0:
         beta = np.finfo(np.float64).tiny
     if (high - most_likely < most_likely - low):